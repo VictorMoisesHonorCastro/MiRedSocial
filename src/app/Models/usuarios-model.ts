0ead2bc0d41f11d7bf06e775182602d387a15b0b
VictorMoisesHonorCastro <vmoisescastro@gmail.com>
2023-01-09 18:26:17 +0100

    commit

---
+++ src/app/Models/usuarios-model.ts
@@ -0,0 +1,10 @@
+export interface Usuarios {
+  nom:string;
+  cognoms:string;
+  edat:string;
+  foto:string;
+  descripcio:string;
+  correu:string;
+  contrasenya:string;
+  confirmarcontrasea:string;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuarios } from './Models/usuarios-model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiRedSocial';
+
+  usuaris: Usuarios[];
+  constructor() {
+    const usuarisJSON: string = `{
+      "usuaris": [
+        {"nom":"Pere","cognoms":"Porras","edat":"25","foto":"https://picsum.photos/300/300","descripcio":"Soltero super guapo y morenote","correu":"pporras@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pilar","cognoms":"Ponas","edat":"18","foto":"https://picsum.photos/300/300","descripcio":"Soltera amable y simpática","correu":"pponas@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pere","cognoms":"Porras","edat":"25","foto":"https://picsum.photos/300/300","descripcio":"Soltero super guapo y morenote","correu":"pporras@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pilar","cognoms":"Ponas","edat":"18","foto":"https://picsum.photos/300/300","descripcio":"Soltera amable y simpática","correu":"pponas@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pere","cognoms":"Porras","edat":"25","foto":"https://picsum.photos/300/300","descripcio":"Soltero super guapo y morenote","correu":"pporras@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pilar","cognoms":"Ponas","edat":"18","foto":"https://picsum.photos/300/300","descripcio":"Soltera amable y simpática","correu":"pponas@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pere","cognoms":"Porras","edat":"25","foto":"https://picsum.photos/300/300","descripcio":"Soltero super guapo y morenote","correu":"pporras@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"},
+        {"nom":"Pilar","cognoms":"Ponas","edat":"18","foto":"https://picsum.photos/300/300","descripcio":"Soltera amable y simpática","correu":"pponas@gmail.com","contrasenya":"123456","confirmarcontrasea":"123456"}
+        ]
+    }`;
+
+    const usuariosDict: any = JSON.parse(usuarisJSON);
+    this.usuaris = usuariosDict['usuaris'];
+  }
+
+  /*selecteUsuarios:Usuarios = new Usuarios();*/
+
 }
+
